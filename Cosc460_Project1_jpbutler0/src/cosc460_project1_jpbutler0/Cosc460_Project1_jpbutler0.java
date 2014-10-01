57cf85068e64b8ba72aa49715671f3dfc2defb65
jackal390iv <jonpbiv@gmail.com>
2014-09-30 23:20:05 -0400

    created java class

---
+++ Cosc460_Project1_jpbutler0/src/cosc460_project1_jpbutler0/Cosc460_Project1_jpbutler0.java
@@ -0,0 +1,19 @@
+package cosc460_project1_jpbutler0;
+
+/**
+ *
+ * @author Jonathan Butler
+ */
+public class Cosc460_Project1_jpbutler0 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new Cosc460_Project1_jpbutler0();
+    }
+    
+    public Cosc460_Project1_jpbutler0(){
+        System.out.println("hello");
+    }
+}
